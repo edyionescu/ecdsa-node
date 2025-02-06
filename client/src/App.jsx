1ca79f73db8dee2c13f70cdb6e304fb22c1f13e2
Edy Ionescu <errorsmith@gmail.com>
2025-02-06 15:23:57 +0200

    Add theme toggle component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Wallet from './Wallet';
 import Transfer from './Transfer';
 import History from './History';
+import ThemeToggle from './ThemeToggle';
 
 function App() {
   const [balance, setBalance] = useState(0);
@@ -24,23 +25,30 @@ function App() {
   }, []);
 
   return (
-    <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 mt-10">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-        <Wallet balance={balance} setBalance={setBalance} setSender={setSender} />
+    <>
+      <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 mt-10">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+          <Wallet balance={balance} setBalance={setBalance} setSender={setSender} />
 
-        <Transfer
-          sender={sender}
-          balance={balance}
-          setBalance={setBalance}
-          transfers={transfers}
-          setTransfers={setTransfers}
-        />
+          <Transfer
+            sender={sender}
+            balance={balance}
+            setBalance={setBalance}
+            transfers={transfers}
+            setTransfers={setTransfers}
+          />
 
-        <div className="lg:col-span-2">
-          <History selectedAddress={sender} transfers={transfers} />
+          <div className="lg:col-span-2">
+            <History selectedAddress={sender} transfers={transfers} />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="absolute right-5 top-5 z-1">
+        <div className="pointer-events-auto">
+          <ThemeToggle />
+        </div>
+      </div>
+    </>
   );
 }
 
